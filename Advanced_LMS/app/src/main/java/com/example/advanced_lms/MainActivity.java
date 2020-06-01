39c27344826b84b734af857961ac863026d1d868
JeongDongHwa <sskk9292@gmail.com>
2020-06-02 01:23:25 +0900

    add Login_activity

--- Advanced_LMS/app/src/main/java/com/example/advanced_lms/MainActivity.java
+++ Advanced_LMS/app/src/main/java/com/example/advanced_lms/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.advanced_lms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         WebLogic w = new WebLogic("id", "pw"); // id , password
         w.attemptLogin();
