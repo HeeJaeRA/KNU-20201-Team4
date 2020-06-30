85080dad9012a00d9967f18b43e26f70cd7fd424
sskk9292 <sskk9292@gmail.com>
2020-06-30 13:17:02 +0900

    implement_Ad_Banner, update app icon

--- Advanced_LMS/app/src/main/java/com/example/advanced_lms/GroupSelectActivity.java
+++ Advanced_LMS/app/src/main/java/com/example/advanced_lms/GroupSelectActivity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
 public class GroupSelectActivity extends AppCompatActivity {
     @Override
@@ -32,6 +36,9 @@ public class GroupSelectActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        ImageView banner = (ImageView) findViewById(R.id.banner2);
+        GlideDrawableImageViewTarget gifImage = new GlideDrawableImageViewTarget(banner);
+        Glide.with(this).load(R.drawable.banner).into(gifImage);
 
 
 //        Button imageButton = (Button) findViewById(R.id.btn_Grouprecommend);
--- Advanced_LMS/app/src/main/java/com/example/advanced_lms/SelectActivity.java
+++ Advanced_LMS/app/src/main/java/com/example/advanced_lms/SelectActivity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
 public class SelectActivity extends AppCompatActivity {
     @Override
@@ -32,6 +36,12 @@ public class SelectActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        ImageView banner = (ImageView) findViewById(R.id.banner);
+        GlideDrawableImageViewTarget gifImage = new GlideDrawableImageViewTarget(banner);
+        Glide.with(this).load(R.drawable.banner).into(gifImage);
+
+
 
 //        buttonGroup.setOnClickListener(new View.OnClickListener() {
 //            @Override
